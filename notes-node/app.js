7bae099da66c1764da14ae94e434a022ed6397e8
Drago96 <dragproychev@gmail.com>
2018-03-01 23:59:41 +0200

    Initial notes-node app creation

---
+++ notes-node/app.js
@@ -0,0 +1,25 @@
+console.log("Starting app.js...");
+
+const fs = require("fs");
+const _ = require("lodash");
+
+const notes = require("./notes");
+
+console.log(process.argv);
+
+const userArguments = process.argv.slice(2);
+const command = userArguments[0];
+console.log("Command:", command);
+
+if (command === "add") {
+    console.log("Adding new note");
+} else if (command === "list") {
+    console.log("Listing all notes");
+} else if (command === "read") {
+    console.log("Reading note");
+} else if (command === "remove") {
+    console.log("Removing note");
+} else {
+    console.log("Command not recognised.");
+}
+
