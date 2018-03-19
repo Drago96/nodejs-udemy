39b2f49bba361f1cd2eb1030b4259fcffd05687f
Drago96 <dragproychev@gmail.com>
2018-03-19 16:21:00 +0200

    Added express to todo project and simple post method for todos

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -1,75 +1,26 @@
-const mongoose = require("mongoose");
+const express = require("express");
+const bodyParser = require("body-parser");
 
-mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://localhost:27017/TodoApp");
+const { mongoose } = require("./db/mongoose");
+const { Todo } = require("./models/todo");
+const { User } = require("./models/user");
 
-const Todo = mongoose.model("Todo", {
-    text: {
-        type: String,
-        required: true,
-        minlength: 1,
-        trim: true
-    },
-    completed: {
-        type: Boolean,
-        default: false
-    },
-    completedAt: {
-        type: Number,
-        default: null
-    }
-});
+const app = express();
 
-// const newTodo = new Todo({
-//     text: "Cook dinner"
-// });
+app.use(bodyParser.json());
 
-// newTodo.save().then((doc) => {
-//     console.log("Saved todo", doc);
-// }, (e) => {
-//     console.log("Unable to save todo", e);
-// });
-
-// const todoWithAllProperties = new Todo({
-//     text: "Take an exam",
-//     completed: true,
-//     completedAt: 123
-// });
-
-// todoWithAllProperties.save()
-//     .then((doc) => {
-//         console.log(JSON.stringify(doc, undefined, 2));
-//     }, (e) => {
-//         console.log("Unable to save todo", e);
-//     });
-
-// const todoWithValidators = new Todo({
-//     text: "Walk the dog"
-// });
-
-// todoWithValidators.save()
-//     .then((doc) => {
-//         console.log(JSON.stringify(doc, undefined, 2));
-//     }, (e) => {
-//         console.log("Unable to save to do", e);
-//     });
-
-const User = mongoose.model("User", {
-    email: {
-        type: String,
-        required: true,
-        minlength: 1,
-        trim: true
-    }
-});
-
-const myUser = new User({
-    email: "  drago@gmail.com  "
-});
-
-myUser.save()
-    .then((doc) => {
-        console.log(JSON.stringify(doc, undefined, 2));
-    }, (e) => {
-        console.log("Unable to save user", e);
+app.post("/todos", (req, res) => {
+    const todo = new Todo({
+        text: req.body.text
     });
+
+    todo.save().then((doc) => {
+        res.send(doc);
+    }, (e) => {
+        res.status(400).send(e);
+    });
+});
+
+app.listen(3000, () => {
+    console.log("Started on port 3000");
+});
