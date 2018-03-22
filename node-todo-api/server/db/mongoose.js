c65167e4e72eea21680e1650609c584771067d37
Drago96 <dragproychev@gmail.com>
2018-03-22 20:05:09 +0200

    Prepared todo app for heroku deployment

--- node-todo-api/server/db/mongoose.js
+++ node-todo-api/server/db/mongoose.js
@@ -1,7 +1,9 @@
 const mongoose = require("mongoose");
 
 mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://localhost:27017/TodoApp");
+const mongooseUrl = process.env.MONGODB_URI
+    || "mongodb://localhost:27017/TodoApp";
+mongoose.connect(mongooseUrl);
 
 module.exports = {
     mongoose
